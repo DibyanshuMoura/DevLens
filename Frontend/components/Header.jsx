d168b4de7674fbf932146ceccacca861fe5fafff
Dibyanshu Moura <dibyanshumouraofficial@gmail.com>
2026-06-21 22:21:49 +0530

    Image Imported

--- Frontend/components/Header.jsx
+++ Frontend/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
+import githubLogo from "../assets/github.svg";
+import xLogo from "../assets/x.svg";
+import devlensLogo from "../assets/favicon/DevLens.png";
+
 const Header = () => {
   return (
     <nav className="w-full py-4 flex items-center justify-between flex-col border-b px-15 gap-4 md:flex-row lg:flex-row sm:flex-row">
       <div className="flex items-center gap-2">
         <img
-          src="/assets/favicon/DevLens.png"
+          src={devlensLogo}
           alt="DevLens Logo"
           className="h-12 mr-2 rounded-full border"
         />
@@ -13,10 +17,16 @@ const Header = () => {
       </div>
       <div className="flex items-center gap-5">
         <a href="https://github.com/DibyanshuMoura" target="_blank">
-          <img src="../assets/github.svg" alt="Github Logo" className="h-10" />
+          <img src={githubLogo} 
+          alt="Github Logo" 
+          className="h-10"
+          />
         </a>
         <a href="https://x.com/DibyanshuMoura" target="_blank">
-          <img src="../assets/x.svg" alt="Twitter Logo" className="h-10" />
+          <img src={xLogo} 
+          alt="Twitter Logo" 
+          className="h-10" 
+          />
         </a>
       </div>
     </nav>
